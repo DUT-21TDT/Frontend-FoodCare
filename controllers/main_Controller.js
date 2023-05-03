96c14be0f3a7e182a507035a28020c752918b5ea
PenguinBKDN <son72ltv2@gmail.com>
2023-05-03 11:30:03 +0700

    more link and modify css

--- controllers/main_Controller.js
+++ controllers/main_Controller.js
@@ -9,4 +9,27 @@ router.get("/test", (req, res, next) => {
     });
 });
 
+router.get("/profile", (req, res, next) => {
+    res.render("pages/MyProfile", {
+        layout: './layouts/main_layout.ejs',
+        title: "My Profile"
+    });
+});
+
+router.get("/myMenu", (req, res, next) => {
+    res.render("pages/MyMenu", {
+        layout: './layouts/main_layout.ejs',
+        title: "My Menu"
+    });
+})
+
+router.get("/createMenu", (req, res, next) => {
+    res.render("pages/CreateYourMenu", {
+        layout: './layouts/main_layout.ejs',
+        title: "Create Menu"
+    });
+})
+
+
+
 module.exports = router;
