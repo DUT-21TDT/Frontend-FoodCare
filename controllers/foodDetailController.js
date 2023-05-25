53b0a9857f355b0b4a0652e7911d9b2197243685
PenguinBKDN <son72ltv2@gmail.com>
2023-05-25 19:21:38 +0700

    holding

--- controllers/foodDetailController.js
+++ controllers/foodDetailController.js
@@ -7,9 +7,7 @@ const renderFoodDetailView = async (req, res, next) => {
 
     const foodInfo = await getFoodDetailById(foodId);
 
-    console.log(foodInfo);
-
-    res.render("pages/Food_information_detail", {
+    res.render("pages/FID_New_V2", {
         layout: './layouts/main_layout.ejs',
         title: "Detail food",
         data: foodInfo.data,
@@ -31,13 +29,67 @@ const renderMenuDetailView = async (req, res, next) => {
 
     const menuInfo = await getMenuDetailById(menuId);
 
+    var nutrition = {
+        energy: 0,
+        carbs: 0,
+        lipid: 0,
+        protein: 0,
+        vitamins: "",
+        minerals: "",
+    };
+
+    var foodElements = [];
+    var index = 0;
+    await Promise.all(
+        menuInfo.data.foods.list.map(async (food) => {
+            const foodInfo = await getFoodDetailById(food.foodid);
+            nutrition.energy += foodInfo.data.energy;
+            nutrition.carbs += foodInfo.data.carbohydrate;
+            nutrition.lipid += foodInfo.data.lipid;
+            nutrition.protein += foodInfo.data.protein;
+
+            if (foodInfo.data.vitamins) {
+                const vitamins = foodInfo.data.vitamins.split(',').map((vitamin) => vitamin.trim());
+                vitamins.forEach((vitamin) => {
+                    if (!nutrition.vitamins.includes(vitamin)) {
+                        nutrition.vitamins += (nutrition.vitamins.length > 0 ? ", " : "") + vitamin;
+                    }
+                });
+            }
+
+            if (foodInfo.data.minerals) {
+                const minerals = foodInfo.data.minerals.split(',').map((mineral) => mineral.trim());
+                minerals.forEach((mineral) => {
+                    if (!nutrition.minerals.includes(mineral)) {
+                        nutrition.minerals += (nutrition.minerals.length > 0 ? ", " : "") + mineral;
+                    }
+                });
+            }
+
+            // Add foodName and foodImage to foodElements
+            foodElements[index] = {
+                foodName: foodInfo.data.foodname,
+                foodAmount: food.amount,
+                foodId: foodInfo.data.foodid,
+            };
+
+            index++;
+        })
+    );
+
+    console.log(foodElements);
 
     res.render("pages/detailMenu", {
         layout: './layouts/main_layout.ejs',
         title: "Detail menu",
         data: menuInfo.data,
+        nutrition: nutrition,
+        foodElements: foodElements,
     });
 };
+
+
+
 
 const getMenuDetailById = async (id) => {
     try {
